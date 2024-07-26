b0871921334313490db7a58c0797025e0ab65395
Triloworld <2671726+Triloworld@users.noreply.github.com>
2024-07-26 09:31:27 +0200

    Init

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full h-full m-6">
+    <NuxtRouteAnnouncer />
+    <h1 class="text-white text-2xl">Mancaft Media Repository</h1>
+    <NuxtImg class="p-6" src="/logo/logo_3_white.png" />
+    <div class="bg-white">
+      <NuxtImg class="p-6" src="/logo/logo_3_black.png" />
+    </div>
+  </div>
+</template>
+
+<style>
+html, body, div#__nuxt{
+  @apply w-full h-full;
+}
+body{
+  @apply bg-black;
+}
+</style>
